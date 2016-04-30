aee18205f6a554127f3af1a58e61bcd615c16ac2
Marco Silva <marcowilliamdf@gmail.com>
2016-04-29 23:26:34 -0300

    removing simplecov from project

--- SisGPWeb/test/test_helper.rb
+++ SisGPWeb/test/test_helper.rb
@@ -1,8 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'simplecov'
-SimpleCov.start
 require 'coveralls'
 Coveralls.wear!
 
