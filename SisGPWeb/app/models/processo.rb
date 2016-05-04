6862cc8d2ef9f3daa3cf35f78504a4e98236839c
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-04 16:43:00 -0300

    Create validations of process

--- SisGPWeb/app/models/processo.rb
+++ SisGPWeb/app/models/processo.rb
@@ -2,4 +2,10 @@ class Processo < ActiveRecord::Base
   has_many :atividades
   has_and_belongs_to_many :donos, class_name: 'Usuario'
 
+  validates :nome, presence: true, length: {maximum: 50}
+  validates :descricao, presence: true, length: {maximum: 200}
+  validades :dataInicio, presence: true
+  validades :dataFim, presence: true
+  
+
 end
