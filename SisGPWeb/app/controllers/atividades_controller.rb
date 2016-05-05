7149788042ba1e4f969566427924d219a8af82df
Marco Silva <marcowilliamdf@gmail.com>
2016-05-05 12:35:57 -0300

    changing message when atividade is created

--- SisGPWeb/app/controllers/atividades_controller.rb
+++ SisGPWeb/app/controllers/atividades_controller.rb
@@ -33,7 +33,7 @@ class AtividadesController < ApplicationController
 
     respond_to do |format|
       if @atividade.save
-        format.html { redirect_to root_path, notice: 'Atividade was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Atividade '+ @atividade.nome + ' criada com sucesso' }
         format.json { render :show, status: :created, location: @atividade }
       else
         format.html { render :new }
