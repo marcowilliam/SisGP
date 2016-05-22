e71b4efb4c5cb74cde0d6ae1f3c4bea92a5dea50
Anna Larissa <annalarissa.martins@gotmial.com>
2016-05-22 20:04:10 -0300

    Changing relations among models

--- SisGPWeb/app/models/portfolio.rb
+++ SisGPWeb/app/models/portfolio.rb
@@ -1,4 +1,16 @@
 class Portfolio < Organizacao
 
 	self.table_name = 'portfolios'
+	has_many :projetos
+	has_and_belongs_to_many :donos, class_name: 'Usuario'
+
+	def adicionar_dono(id)
+    @new_dono = Usuario.find(id)
+    if !self.donos.include?(@new_dono)
+      self.donos << @new_dono
+    else
+      #do nothing
+    end
+  end
+
 end
