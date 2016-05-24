8c0c5aec5308b96409d0841c7215ec476de3bbe8
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-24 20:44:59 -0300

    Create example for email validation

--- SisGPWeb/spec/models/usuario_spec.rb
+++ SisGPWeb/spec/models/usuario_spec.rb
@@ -4,13 +4,19 @@ require "usuario"
 
 describe Usuario do
 	context "validations" do
-		it "requite email to be set"
+		
+		it "requite email to be set" do
+			usuario = Usuario.create
+			usuario.nome = "Test"
+			usuario.email = "a@a.com"
+			usuario.email.should == "a@a.com"
+		end
 
 		it "requite name to be set"
 
 		it "requite login to be set"
 
 		it "requite password to be set"
-		
+
 	end
 end
