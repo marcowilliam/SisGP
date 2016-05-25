5f3ca8e5c54f400e6f789ea363741afc283f5970
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-24 21:01:39 -0300

    Create rspec example for invalid email

--- SisGPWeb/spec/models/usuario_spec.rb
+++ SisGPWeb/spec/models/usuario_spec.rb
@@ -15,8 +15,16 @@ describe Usuario do
 			expect(usuario).to be_valid
 		end
 
-		it "requite right email"
-
+		it "requite right email" do
+			usuario = Usuario.create
+			usuario.nome = "Test"
+			usuario.login = "123456"
+			usuario.telefone = "555"
+			usuario.password_digest = "123"
+			usuario.email = "aaaaa"
+			expect(usuario).to be_invalid
+		end
+		
 		it "requite right login size"
 
 		it "requite password to be set"
