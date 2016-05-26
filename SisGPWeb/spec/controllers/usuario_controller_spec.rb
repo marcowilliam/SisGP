7cc535742cd83b9ab7783efff87c069b91233c32
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-26 16:01:22 -0300

    Refactoring test of user controller

--- SisGPWeb/spec/controllers/usuario_controller_spec.rb
+++ SisGPWeb/spec/controllers/usuario_controller_spec.rb
@@ -6,13 +6,22 @@ require "rails_helper"
 require "usuario"
 require "usuarios_controller"
 
-describe UsuariosController do
-	describe "POST" do
-		it "New User" do
-			@usuario = Usuario.new
-			@usuario.should be_an_instance_of Usuario
+describe UsuariosController, :type => :controller do
+	describe "GET user" do
+		let(:user) {Usuario.create(:nome =>"test", :login=>"123455", :email=>"a@a.com",:password=>"123456", :password_confirmation=>"123456", :telefone=>"123456")}
+
+		before do
+			user
 		end
 
-	end
+		it "new user" do
+			user = Usuario.new
+			expect(user).to be_instance_of(Usuario)
+		end
 
+		it "create user" do
+			user = Usuario.create(:nome =>"test", :login=>"123455", :email=>"a@a.com",:password=>"123456", :password_confirmation=>"123456", :telefone=>"123456")
+			expect(user.nome).to eq("test")
+		end
+	end	
 end
