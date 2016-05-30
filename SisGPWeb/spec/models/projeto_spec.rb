315359741f0360f7e5dda571c5ef6d40b8ceb9f1
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-30 09:26:40 -0300

    Create it of rspec projeto test

--- SisGPWeb/spec/models/projeto_spec.rb
+++ SisGPWeb/spec/models/projeto_spec.rb
@@ -10,5 +10,10 @@ describe Projeto do
 	subject(:projeto) { Projeto.new(nome: "teste", descricao: "teste01", dataInicio: nil, dataFim: nil)}
 	context "validations" do
 		let(:user) {Usuario.create(:nome =>"test", :login=>"123455", :email=>"a@a.com",:password=>"123456", :password_confirmation=>"123456", :telefone=>"123456")}
+
+		it "should add responsable" do
+			projeto.adicionar_dono(user.id)
+			expect((projeto.donos[1])).to equal(nil)
+		end
 	end
 end
