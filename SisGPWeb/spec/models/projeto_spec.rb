f613e3fad76be4c3cd35ba2ca52d3ea994a44690
Rafael Akiyoshi <rafah94@hotmail.com>
2016-05-30 09:23:54 -0300

    Create context of test and let user of rspec

--- SisGPWeb/spec/models/projeto_spec.rb
+++ SisGPWeb/spec/models/projeto_spec.rb
@@ -8,5 +8,7 @@ require 'usuario'
 
 describe Projeto do
 	subject(:projeto) { Projeto.new(nome: "teste", descricao: "teste01", dataInicio: nil, dataFim: nil)}
-
+	context "validations" do
+		let(:user) {Usuario.create(:nome =>"test", :login=>"123455", :email=>"a@a.com",:password=>"123456", :password_confirmation=>"123456", :telefone=>"123456")}
+	end
 end
