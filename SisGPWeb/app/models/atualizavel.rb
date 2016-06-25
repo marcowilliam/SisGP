84dd471c66e31d6d2652596199bd687e9b486dfb
Marco Silva <marcowilliamdf@gmail.com>
2016-06-25 08:57:39 -0300

    removing abstract methods

--- SisGPWeb/app/models/atualizavel.rb
+++ SisGPWeb/app/models/atualizavel.rb
@@ -1,6 +1,3 @@
 class Atualizavel < ActiveRecord::Base
-	self.abstract_class = true
-	require 'abstract_method'
-	
-	abstract_method :aceita_atualizador
+
 end
--- SisGPWeb/app/models/organizacao.rb
+++ SisGPWeb/app/models/organizacao.rb
@@ -1,11 +1,2 @@
 class Organizacao <  Atualizavel 
-	self.abstract_class = true
-	require 'abstract_method'
-
-	abstract_method :adicionar_dono
-	abstract_method :dataInicio_nao_pode_ser_no_passado
-	abstract_method :dataFim_nao_pode_ser_menor_que_DataInicio
-	abstract_method :aceita_atualizador
-
-
 end
